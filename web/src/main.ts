0a30a8199c9633e5bfef015b5607ff275f49d9dd
datwaft <datwaftx@gmail.com>
2021-03-16 11:39:02 -0600

    Add PrimeVue dependency

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,12 @@
+// VueJS
 import { createApp } from 'vue'
 import App from './App.vue'
+// Vue Router
 import router from './router'
+// PrimeVue
+import PrimeVue from 'primevue/config'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(PrimeVue)
+  .mount('#app')
