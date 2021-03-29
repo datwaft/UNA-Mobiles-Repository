dae6f1bdb9e943ec373bde68e917b4c5d3d81cff
datwaft <datwaftx@gmail.com>
2021-03-28 21:42:15 -0600

    Update setup.bat

--- database/setup.bat
+++ database/setup.bat
@@ -8,4 +8,6 @@ psql -U postgres -f "create_tables.sql"
 echo.
 psql -U postgres -f "populate_tables.sql"
 echo.
+psql -U postgres -f "create_views.sql"
+echo.
 psql -U postgres -f "create_procedures.sql"
