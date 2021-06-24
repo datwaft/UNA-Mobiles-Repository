a2b202b52f90e352040c867ae4f701fab7531238
datwaft <datwaftx@gmail.com>
2021-06-23 23:34:12 -0600

    feat(mobile): example using echo

---
+++ mobile/app/src/main/java/org/una/mobile/Globals.kt
@@ -0,0 +1,4 @@
+package org.una.mobile
+
+const val WEB_SOCKET_URL = "echo.websocket.org"
+//const val WEB_SOCKET_PORT = 8099
--- mobile/app/src/main/java/org/una/mobile/MainActivity.kt
+++ mobile/app/src/main/java/org/una/mobile/MainActivity.kt
@@ -3,20 +3,38 @@ package org.una.mobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import org.una.mobile.client.WebSocketClient
 import org.una.mobile.ui.theme.MobileTheme
+import org.una.mobile.viewmodel.MessageViewModel
 
 class MainActivity : ComponentActivity() {
+    override fun onResume() {
+        super.onResume()
+        WebSocketClient.connect(lifecycleScope)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        WebSocketClient.close()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MobileTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    App(Modifier.animateContentSize())
                 }
             }
         }
@@ -24,14 +42,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun App(modifier: Modifier = Modifier) {
+    Content(modifier)
 }
 
-@Preview(showBackground = true)
+// TODO: Delete this test composable
 @Composable
-fun DefaultPreview() {
-    MobileTheme {
-        Greeting("Android")
+private fun Content(modifier: Modifier = Modifier) {
+    val viewModel: MessageViewModel = viewModel()
+    var text: String by remember { mutableStateOf("") }
+
+    Column(modifier) {
+        LazyColumn(Modifier
+            .padding(16.dp)
+            .weight(1f),
+            verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            items(viewModel.items) { item ->
+                Card(Modifier.fillMaxWidth()) { Text(item, Modifier.padding(8.dp)) }
+            }
+        }
+        Row(Modifier.padding(32.dp).fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            OutlinedTextField(text, { text = it }, Modifier.weight(1f).padding(end = 16.dp))
+            Button(onClick = { viewModel.send(text) }) { Text("SEND") }
+        }
     }
 }
